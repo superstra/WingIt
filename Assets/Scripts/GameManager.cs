8572f1d97333215e3f02b67325922cd7642ecdb7
Carter Edginton <cmedginton@gmail.com>
2024-11-16 23:49:16 -0700

    Added functionality for match start (WIP)
    logic unimplemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,17 +11,25 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text timerText;
     [SerializeField] private TMP_Text speedText;
+    [SerializeField] private TMP_Text countdownText;
     [SerializeField] public bool activeTimer = false;
     [SerializeField] private Rigidbody bird;
+
+    private bool gamestarted = false;
+    private float countDownTimeInterval = 1.0f;
+    private float countdowntimeleft = 1.0f;
+
     float startTime;
     float time;
     // Start is called before the first frame update
     void Start()
     {
+        gamestarted = false;
         activeTimer = false;
         startTime = Time.time;
         timerText.text = startTime.ToString();
         speedText.text = "hi hello im init'd";
+        countdownText.text = "4";
     }
 
     // Update is called once per frame
@@ -46,5 +54,42 @@ public class GameManager : MonoBehaviour
         //float currtime = Time.time - startTime;
         //currtime = MathF.Round(currtime, 3);    
         //text.text = currtime.ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!gamestarted) {
+            if (countdowntimeleft <= 0f) {
+                countdowntimeleft = countDownTimeInterval;
+                if (countdownText.text == "4") 
+                {
+                    countdownText.text = "3"; 
+                }
+                else if (countdownText.text == "3")
+                {
+                    countdownText.text = "2";
+                }
+                else if (countdownText.text == "2")
+                {
+                    countdownText.text = "1";
+                }
+                else if (countdownText.text == "1")
+                {
+                    countdownText.text = "GO!";
+                    //let player move here
+                }
+                else
+                {
+                    countdownText.text = "";
+                }
+            }
+            else
+            {
+                countdowntimeleft -= Time.fixedDeltaTime;
+            }
+
+
+
+        }
     }
 }
