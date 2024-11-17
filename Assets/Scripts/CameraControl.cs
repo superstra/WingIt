f580e3297c766e92c1465d2d7f9a2484852a8915
Carter Edginton <cmedginton@gmail.com>
2024-11-17 14:41:21 -0700

    Further fixes to camera orbit
    Rotating in real time will work if you uncomment the updateCamOffset call in Update

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -18,7 +18,9 @@ public class CameraControl : MonoBehaviour
 
     void updateCamOffset() {
         Vector3 angles = cam.transform.rotation.eulerAngles;
-        camOffset = new Vector3(Mathf.Sin(angles.y * Mathf.Deg2Rad), -1F * Mathf.Sin(angles.x * Mathf.Deg2Rad), Mathf.Cos(angles.x * Mathf.Deg2Rad));
+        camOffset = new Vector3(Mathf.Sin(angles.y * Mathf.Deg2Rad)* Mathf.Cos(angles.x * Mathf.Deg2Rad),
+                                -1F * Mathf.Sin(angles.x * Mathf.Deg2Rad), 
+                                Mathf.Cos(angles.x * Mathf.Deg2Rad) * Mathf.Cos(angles.y*Mathf.Deg2Rad));
         camOffset.Normalize();
         camOffset *= -distanceFromBird;
     }
@@ -28,6 +30,8 @@ public class CameraControl : MonoBehaviour
     {
         //float objx = objectPosition.position.x, objy = objectPosition.position.y, objz = objectPosition.position.z;
         //cam.transform.SetPositionAndRotation(new Vector3(objx, objy + 13, objz-5), cam.transform.rotation);
+        
+        //updateCamOffset();
         cam.transform.SetPositionAndRotation(objectPosition.position + camOffset, cam.transform.rotation);
     }
 }
